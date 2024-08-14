b2c520ad62cb427d5147a0382ad20bb026998153
Kode Sonik <95509615+kodesonik@users.noreply.github.com>
2024-08-14 11:57:46 +0000

    Fix build bugs

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,28 +1,35 @@
-import { Metadata } from 'next';
-import Layout from '../../layout/layout';
+import { Metadata, Viewport } from "next";
+import Layout from "../../layout/layout";
 
 interface MainLayoutProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
+export const viewport: Viewport = {
+  themeColor: "dark",
+  initialScale: 1,
+  width: "device-width",
+};
+
 export const metadata: Metadata = {
-    title: 'PrimeReact Atlantis',
-    description: 'The ultimate collection of design-agnostic, flexible and accessible React UI Components.',
-    robots: { index: false, follow: false },
-    viewport: { initialScale: 1, width: 'device-width' },
-    openGraph: {
-        type: 'website',
-        title: 'PrimeReact Atlantis-REACT',
-        url: 'https://www.primefaces.org/Atlantis-react',
-        description: 'The ultimate collection of design-agnostic, flexible and accessible React UI Components.',
-        images: ['https://www.primefaces.org/static/social/Atlantis-react.png'],
-        ttl: 604800
-    },
-    icons: {
-        icon: '/favicon.ico'
-    }
+  title: "BlackBase",
+  description:
+    "The ultimate collection of design-agnostic, flexible and accessible React UI Components.",
+  robots: { index: false, follow: false },
+  openGraph: {
+    type: "website",
+    title: "PrimeReact Atlantis-REACT",
+    url: "https://www.primefaces.org/Atlantis-react",
+    description:
+      "The ultimate collection of design-agnostic, flexible and accessible React UI Components.",
+    images: ["https://www.primefaces.org/static/social/Atlantis-react.png"],
+    ttl: 604800,
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function MainLayout({ children }: MainLayoutProps) {
-    return <Layout>{children}</Layout>;
+  return <Layout>{children}</Layout>;
 }
